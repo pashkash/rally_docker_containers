207c3593d51b5486c44fff30a6d5fb6a38a50855
root <root@cfg01.mcp10-scale-lab-ovs.local>
2017-05-27 15:19:47 +0000

    fixes & comments in install.sh

--- rally_longev/install.sh
+++ rally_longev/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# This file create necessary files for working langevitydocker container.
 
-cp /etc/ssl/certs/ca-certificates.crt ./resources/$PWD
+#1. download image with ubuntu
+wget -P ./resources/ https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img 
 
+#2. copy cert file
+cp /etc/ssl/certs/ca-certificates.crt ./resources/
+
+#3. make&store node config file
 HOME_CONFIG="$(pwd)/resources/rackspacetruck.cfg"
 
 echo "[nodes]" > "${HOME_CONFIG}"
@@ -18,3 +24,10 @@ for i in $(salt-run manage.status | grep " - " | grep "cmp\|ctl" | awk -F  " - "
   esac
 done
 
+#4. now we can run container from public repo (or we can build it with "docker build --no-cache=true -t longevity .")
+#docker run -v html_reports:/home/rally/rally_arts/ \
+#    -v $PWD/resources/:/home/rally/resources/ \
+#    -v ~/.ssh:/root/.ssh \
+#    -e OS_AUTH_URL=http://10.167.4.10:35357/v2.0 \
+#    --net host \
+#    -it pashkash/longevity
