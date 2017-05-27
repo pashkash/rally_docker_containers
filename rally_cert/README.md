da305ea69a74cfca0c9dff49f0f16768a4f25622
root <root@cfg01.mcp10-scale-lab-ovs.local>
2017-05-27 15:47:46 +0000

    fixes & comments in install.sh

--- rally_cert/README.md
+++
@@ -1,1 +0,0 @@
-docker run -v html_rally_reports:/home/rally/rally_arts/ -v ~/.ssh:/root/.ssh --net host -it perf-rally
--- rally_cert/install.sh
+++ rally_cert/install.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-set -eu -o pipefail
+# This file create necessary files for working langevitydocker container.
 
-HOME_CONFIG="${HOME}/.rackspacetruck.cfg"
+#1. copy cert file
+cp /etc/ssl/certs/ca-certificates.crt ./resources/
 
-echo "[nodes]" > "${HOME_CONFIG}"
-for i in $(salt-run manage.status | grep " - " | grep "cmp\|ctl" | awk -F  " - " '{print $2}'); do
-  IFS='.' read -r -a str <<< ${i}
-  number=${str[0]:3}
-  case "${i}" in
-    *"ctl"*)
-        echo "controller_$((number+0)) = ${i}" >> "${HOME_CONFIG}"
-        ;;
-    *"cmp"*)
-        echo "compute_$((number+0)) = ${i}" >> "${HOME_CONFIG}"
-        ;;
-  esac
+#2. now we can run container from public repo (or we can build it with "docker build --no-cache=true -t rally_certification .")
+#docker run -v html_reports:/home/rally/rally_arts/ \
+#    -v $PWD/resources/:/home/rally/resources/ \
+#    -v ~/.ssh:/root/.ssh \
+#    -e OS_AUTH_URL=http://10.167.4.10:35357/v2.0 \
+#    --net host \
+#    -it pashkash/rally_certification
